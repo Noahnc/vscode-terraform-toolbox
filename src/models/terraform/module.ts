31bc89fa45477a1a139967277e9b4b85f0ac9298
Noahnc <nc.canadea@gmail.com>
2023-08-07 21:09:38 +0200

    fix(TerraformResource): Make version optional.

--- src/models/terraform/module.ts
+++ src/models/terraform/module.ts
@@ -3,7 +3,7 @@ import { TerraformBaseResource } from "./base_resource";
 export class Module implements TerraformBaseResource {
   key: string;
   source: string;
-  version: string;
+  version?: string;
 
   constructor(key: string, source: string, version: string) {
     this.key = key;
