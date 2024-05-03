e48151c5636e8d73068f4ca41cb33c712f318f5f
Noah Canadea <noahnc@MacBook-Pro-von-Noah.local>
2024-05-03 11:40:43 +0200

    feat(settings): Add event handler to notify user about required restart after changing some settings

--- src/models/settings.ts
+++ src/models/settings.ts
@@ -2,47 +2,107 @@ import * as vscode from "vscode";
 import { getLogger } from "../utils/logger";
 
 export class Settings {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  getVSCodeSettingKey(key: string, allowUndefined = false): any {
-    const setting = vscode.workspace.getConfiguration().get(key);
-    getLogger().trace("Got setting " + key + ": " + setting);
-    if (setting === undefined && !allowUndefined) {
-      throw new Error("Setting " + key + " is undefined");
-    }
-    return setting;
+  private _settingsDict: { [key: string]: SettingsElement } = {};
+
+  constructor() {
+    this._settingsDict["spaceliftTenantID"] = new SettingsElement("tftoolbox.spacelift.tenantID", true, true);
+    this._settingsDict["spacectlProfileName"] = new SettingsElement("tftoolbox.spacelift.profileName", true, true);
+    this._settingsDict["autoselectVersion"] = new SettingsElement("tftoolbox.terraform.autoSelectVersion");
+    this._settingsDict["logLevel"] = new SettingsElement("tftoolbox.logLevel");
+    this._settingsDict["autoSelectWorkspace"] = new SettingsElement("tftoolbox.terraform.autoSelectWorkspace");
+    this._settingsDict["autoInitAllProjects"] = new SettingsElement("tftoolbox.terraform.autoInitAllProjects");
+    this._settingsDict["initArgs"] = new SettingsElement("tftoolbox.terraform.initArg");
+    this._settingsDict["spaceliftStatusBarItemRefreshIntervalSeconds"] = new SettingsElement("tftoolbox.spacelift.stackPendingConfirmationStatusItemUpdateTimeSeconds");
+    this._settingsDict["excludedGlobPatterns"] = new SettingsElement("tftoolbox.excludeGlobPatterns");
+    this._settingsDict["showSpacectlNotAuthenticatedWarningOnStartup"] = new SettingsElement("tftoolbox.spacelift.showLoginNotificationOnStartup");
+    this._settingsDict["useOpenTofuInsteadOfTerraform"] = new SettingsElement("tftoolbox.UseOpenTofuInsteadOfTerraform", true);
+
+    vscode.workspace.onDidChangeConfiguration(this.handleSettingChange.bind(this));
   }
 
   get spaceliftTenantID(): string | undefined {
-    return this.getVSCodeSettingKey("tftoolbox.spacelift.tenantID", true);
+    return this._settingsDict["spaceliftTenantID"].value;
   }
   get spacectlProfileName(): string | undefined {
-    return this.getVSCodeSettingKey("tftoolbox.spacelift.profileName", true);
+    return this._settingsDict["spacectlProfileName"].value;
   }
   get autoselectVersion(): boolean {
-    return this.getVSCodeSettingKey("tftoolbox.terraform.autoSelectVersion");
+    return this._settingsDict["autoselectVersion"].value;
   }
   get logLevel(): string {
-    return this.getVSCodeSettingKey("tftoolbox.logLevel");
+    return this._settingsDict["logLevel"].value;
   }
   get autoSelectWorkspace(): boolean {
-    return this.getVSCodeSettingKey("tftoolbox.terraform.autoSelectWorkspace");
+    return this._settingsDict["autoSelectWorkspace"].value;
   }
   get autoInitAllProjects(): boolean {
-    return this.getVSCodeSettingKey("tftoolbox.terraform.autoInitAllProjects");
+    return this._settingsDict["autoInitAllProjects"].value;
   }
   get initArgs(): string {
-    return this.getVSCodeSettingKey("tftoolbox.terraform.initArg");
+    return this._settingsDict["initArgs"].value;
   }
   get spaceliftStatusBarItemRefreshIntervalSeconds(): number {
-    return this.getVSCodeSettingKey("tftoolbox.spacelift.stackPendingConfirmationStatusItemUpdateTimeSeconds");
+    return this._settingsDict["spaceliftStatusBarItemRefreshIntervalSeconds"].value;
   }
   get excludedGlobPatterns(): string[] {
-    return this.getVSCodeSettingKey("tftoolbox.excludeGlobPatterns");
+    return this._settingsDict["excludedGlobPatterns"].value;
   }
   get showSpacectlNotAuthenticatedWarningOnStartup(): boolean {
-    return this.getVSCodeSettingKey("tftoolbox.spacelift.showLoginNotificationOnStartup");
+    return this._settingsDict["showSpacectlNotAuthenticatedWarningOnStartup"].value;
   }
   get useOpenTofuInsteadOfTerraform(): boolean {
-    return this.getVSCodeSettingKey("tftoolbox.UseOpenTofuInsteadOfTerraform");
+    return this._settingsDict["useOpenTofuInsteadOfTerraform"].value;
+  }
+
+  handleSettingChange(event: vscode.ConfigurationChangeEvent): void {
+    if (!event.affectsConfiguration("tftoolbox")) {
+      return;
+    }
+    Object.values(this._settingsDict).forEach((element) => {
+      element.handleSettingChange(event);
+    });
+  }
+}
+
+class SettingsElement {
+  private key: string;
+  private changeRequiresRestart: boolean;
+  private allowUndefined;
+
+  constructor(settingsKey: string, changeRequiresRestart = false, allowUndefined = false) {
+    this.key = settingsKey;
+    this.changeRequiresRestart = changeRequiresRestart;
+    this.allowUndefined = allowUndefined;
+  }
+
+  get settingsKey(): string {
+    return this.key;
+  }
+
+  handleSettingChange(event: vscode.ConfigurationChangeEvent): void {
+    if (!this.changeRequiresRestart) {
+      return;
+    }
+    if (event.affectsConfiguration(this.key)) {
+      getLogger().info("Setting " + this.key + " changed and requires restart of the extension.");
+      // show a information with two buttons to restart the extension or skip
+      vscode.window.showInformationMessage("The setting " + this.key + " changed and requires a restart of the extension.", "Restart", "Skip").then((value) => {
+        if (value === "Restart") {
+          vscode.commands.executeCommand("workbench.action.reloadWindow");
+        }
+      });
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  get value(): any {
+    const setting = vscode.workspace.getConfiguration().get(this.key);
+
+    getLogger().trace("Got setting " + this.key + ": " + setting);
+    if (setting === undefined && !this.allowUndefined) {
+      throw new Error("Setting " + this.key + " is undefined");
+    }
+
+    return setting;
   }
 }
